7750d83e8c8c3405b2a6d44fdf47ded4c30baa06
michaelbagwell <mlbagwell1@gmail.com>
2014-09-01 23:46:10 -0400

    Update lightbox.js

--- barest/lightbox.js
+++ barest/lightbox.js
@@ -4,10 +4,11 @@ $(document).ready(function($) {
         var href = $(this).attr("href");
         if ($('#lightbox').length > 0) {
           $('#content').html('<img src="' + href + '" />');
-          $('#lightbox').show();
+          $('#lightbox, #background').show();
         }
         else {
             var lightbox =
+                    '<div id="background"></div>' +
                     '<div id="lightbox">' +
                     '<div id="content">' + //
                     '<img src="' + href + '" />' +
@@ -15,8 +16,8 @@ $(document).ready(function($) {
                     '</div>';
             $('body').append(lightbox);
         }
-        $('#lightbox').live('click', function() {
-            $('#lightbox').hide();
+        $('#lightbox').on('click', function() {
+            $('#lightbox, #background').hide();
         });
     })
 });
